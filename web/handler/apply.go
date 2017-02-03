9e442b62b5b567583423690ea569e7a6211deaf3
Jannis Fink <fink@uni-bremen.de>
2017-02-03 09:56:04 +0100

    serve static files from layout/static

--- web/handler/apply.go
+++ web/handler/apply.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/suluvir/server/web"
 	"github.com/suluvir/server/web/httpHelpers"
+	"net/http"
 )
 
 func init() {
@@ -10,4 +11,5 @@ func init() {
 	router.HandleFunc("/", IndexHandler).Methods(httpHelpers.GET)
 	router.HandleFunc("/upload", UploadPageHandler).Methods(httpHelpers.GET)
 	router.HandleFunc("/upload", SongUploadHandler).Methods(httpHelpers.POST)
+	router.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("layout/static/"))))
 }
