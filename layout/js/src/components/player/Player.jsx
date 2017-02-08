cc75c438f2263d8c5b1c5f0bbcd314bdf4dc332a
Jannis Fink <fink@uni-bremen.de>
2017-02-08 22:03:05 +0100

    removed colors from webpage

--- layout/js/src/components/player/Player.jsx
+++ layout/js/src/components/player/Player.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-require('./Player.scss');
-
 export default class Player extends React.Component {
     render() {
         return (
             <div id="suluvir-player">
-                {'result'}
             </div>
         );
     }
