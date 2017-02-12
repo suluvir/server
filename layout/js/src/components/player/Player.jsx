9572bf7df285c96aa44bb4dd2b55558e8235a76a
Jannis Fink <fink@uni-bremen.de>
2017-02-12 22:47:39 +0100

    basic song streaming works

--- layout/js/src/components/player/Player.jsx
+++ layout/js/src/components/player/Player.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import Immutable from 'immutable';
+import {connect} from 'react-redux';
+import classNames from 'classnames';
 
-export default class Player extends React.Component {
+require('./Player.scss');
+
+class Player extends React.Component {
     render() {
+        const {songs} = this.props;
+
+        const className = classNames('suluvir-player', {'suluvir-player--active': songs.size > 0});
+        if (songs.size == 0) {
+            return (
+                <div id="suluvir-player" className={className}>
+                    test
+                </div>
+            );
+        }
+
+        const songToPlay = songs.first();
+
         return (
-            <div id="suluvir-player">
+            <div id="suluvir-player" className={className}>
+                test
+                <audio id="demo" src={songToPlay.get('@stream')} autoPlay>
+
+                </audio>
             </div>
         );
     }
 }
+
+Player.propTypes = {
+    songs: React.PropTypes.instanceOf(Immutable.List).isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        songs: state.play
+    };
+}
+
+export default connect(mapStateToProps)(Player);
