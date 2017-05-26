9a6fa77f7e716e071b884bb240a15395191163c0
Jannis Fink <fink@uni-bremen.de>
2017-05-27 00:47:05 +0200

    add basic file upload handler, file upload is not working

--- layout/js/src/components/upload/Upload.jsx
+++ layout/js/src/components/upload/Upload.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
+
+require('./Upload.scss');
 
 export default class Upload extends React.PureComponent {
+    constructor() {
+        super();
+
+        this.handleDrop = this.handleDrop.bind(this);
+    }
+
+    handleDrop(acceptedFiles, rejectedFiles) {
+        this.setState({
+            acceptedFiles,
+            rejectedFiles
+        });
+    }
+
     render() {
-        return <div/>;
+        return (
+            <div className="suluvir-upload">
+                <div className="suluvir-upload__dropzone">
+                    <Dropzone 
+                        accept="audio/mp3"
+                        className="suluvir-upload__dropzone mdl-shadow--2dp"
+                        onDrop={this.handleDrop}
+                    >
+                        Drop some files here or click here to select the songs to upload.
+                    </Dropzone>
+                </div>
+            </div>
+        );
     }
 }
