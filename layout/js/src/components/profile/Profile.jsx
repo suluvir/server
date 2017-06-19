2a25d3abcbc61dafcd7359cca70c5609d148c7e6
Jannis Fink <fink@uni-bremen.de>
2017-06-19 21:47:26 +0200

    show quota on users profile page

--- layout/js/src/components/profile/Profile.jsx
+++ layout/js/src/components/profile/Profile.jsx
@@ -2,11 +2,35 @@ import React from 'react';
 import Immutable from 'immutable';
 import {connect} from 'react-redux';
 
-import {List, ListItem, ListItemContent} from 'react-mdl';
+import {List, ListItem, ListItemContent, ProgressBar} from 'react-mdl';
 
 import {fetchUser} from '../../actions/thunkActions';
 
+import {formatBytes} from '../../utils/formatters';
+
 class Profile extends React.PureComponent {
+    getQuotaSpacePercentage() {
+        const {user} = this.props;
+        return 100 - (user.get('available_quota_space') / user.get('quota_space') * 100);
+    }
+
+    getQuotaSongPercentage() {
+        const {user} = this.props;
+        return 100 - (user.get('available_quota_songs') / user.get('quota_songs') * 100);
+    }
+
+    getQuotaSongDisplay() {
+        const {user} = this.props;
+        const available = user.get('quota_songs') - user.get('available_quota_songs');
+        return `${available} / ${user.get('quota_songs')}`;
+    }
+
+    getQuotaSpaceDisplay() {
+        const {user} = this.props;
+        const available = user.get('quota_space') - user.get('available_quota_space');
+        return `${formatBytes(available)} / ${formatBytes(user.get('quota_space'))}`;
+    }
+
     componentWillMount() {
         this.props.fetchUser();
     }
@@ -25,6 +49,16 @@ class Profile extends React.PureComponent {
                         <ListItemContent avatar="mail_outline" subtitle="E-Mail">{user.get('email')}</ListItemContent>
                     </ListItem>
                 </List>
+
+                <hr/>
+
+                <h2>Quota</h2>
+
+                Songs ({this.getQuotaSongDisplay()}):
+                <ProgressBar progress={this.getQuotaSongPercentage()} />
+
+                Space ({this.getQuotaSpaceDisplay()}):
+                <ProgressBar progress={this.getQuotaSpacePercentage()} />
             </div>
         );
     }
--- layout/js/src/utils/formatters.jsx
+++ layout/js/src/utils/formatters.jsx
@@ -28,6 +28,15 @@ export function formatTime(duration) {
     return `${minutesStr}:${secondsStr}`;
 }
 
+export function formatBytes(bytes, decimals = 2) {
+    // heavily inspired by https://stackoverflow.com/a/18650828
+    if(bytes == 0) return '0 Bytes';
+    var k = 1024,
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
+}
+
 export function songMenuButton(song) {
     return <SongMenuButton song={song} />;
 }
