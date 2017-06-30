3abd1497681423bb2b94be147e6aaea262de29d3
Jannis Fink <fink@uni-bremen.de>
2017-06-30 22:32:36 +0200

    add a nice message as a html comment

--- web/handler/index.go
+++ web/handler/index.go
@@ -16,22 +16,43 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/suluvir/server/environment"
+	"github.com/suluvir/server/logging"
 	"github.com/suluvir/server/web/dependencyLoader"
 	"github.com/suluvir/server/web/printer"
 	"github.com/suluvir/server/web/setup"
+	"go.uber.org/zap"
+	"html/template"
+	"io/ioutil"
 	"net/http"
+	"path"
 )
 
 type indexTemplate struct {
-	Externals []dependencyLoader.External
-	Setup     string
+	Externals       []dependencyLoader.External
+	Setup           string
+	GreetingMessage template.HTML
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	extractor := dependencyLoader.NewExtractor("layout/js/webpack.config.js", "layout/js/package.json")
 	variables := indexTemplate{
-		Externals: extractor.ExtractExternals(),
-		Setup:     setup.GetSetup(r),
+		Externals:       extractor.ExtractExternals(),
+		Setup:           setup.GetSetup(r),
+		GreetingMessage: greetingMessage(),
 	}
 	printer.PrintHtmlPageFromFile(w, "layout/html/defaulttemplate.html", variables)
+}
+
+func greetingMessage() template.HTML {
+	greetingFile := path.Join(environment.GetBaseDirectory(), "layout", "static", "greetingmessage.txt")
+	buffer, err := ioutil.ReadFile(greetingFile)
+	if err != nil {
+		logging.GetLogger().Error("error reading greeting file", zap.Error(err))
+		return template.HTML("<!--\nthere happened an error, so I'm not able to show a nice message here :(\n-->")
+	}
+
+	message := fmt.Sprintf("<!--\n%s\n-->", string(buffer))
+	return template.HTML(message)
 }
