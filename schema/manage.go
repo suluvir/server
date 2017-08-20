1f88642814fe2df55d8254d028c45a711735a92f
Jannis Fink <fink@uni-bremen.de>
2017-08-20 23:26:05 +0200

    remove sqliet support for the moment, as it does not allow cross compiling

--- schema/manage.go
+++ schema/manage.go
@@ -18,7 +18,7 @@ package schema
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	//	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/suluvir/server/config"
 	"github.com/suluvir/server/environment"
 	"github.com/suluvir/server/logging"
