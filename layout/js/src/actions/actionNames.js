efc62f4cb61b1bfc2cad6711a622fc9630ca31e0
Jannis Fink <fink@uni-bremen.de>
2017-10-09 21:01:45 +0200

    add license header to js files, closes #2

--- layout/js/src/actions/actionNames.js
+++ layout/js/src/actions/actionNames.js
@@ -1,3 +1,18 @@
+// Suluvir streaming server
+// Copyright (C) 2017  Jannis Fink
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 export const SET_MY_SONGS = 'SET_MY_SONGS';
 export const ADD_TO_MY_SONGS = 'ADD_TO_MY_SONGS';
 export const SET_MY_ARTISTS = 'SET_MY_ARTISTS';
